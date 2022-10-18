0c37aa6b18b5b365f4c754c0f6a47cd3cd78d631
DmytroAntonenko <d.antonenko81@gmail.com>
2022-10-18 15:49:09 +0300

    #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+const startBtn = document.querySelector('button[data-start]');
+console.log(startBtn);
+const stopBtn = document.querySelector('button[data-stop]');
+console.log(stopBtn);
+const bodyRef = document.querySelector('body');
+let timerId = null;
+startBtn.disabled = false;
+
+startBtn.addEventListener('click', changeBodyColor);
+
+function changeBodyColor (event) {
+  if(startBtn.disabled = true) {
+  timerId = setInterval(() => 
+  { bodyRef.style.backgroundColor = getRandomHexColor(); }, 1000)
+  }
+  return;
+};
+
+stopBtn.addEventListener('click', removeBodyColor);
+function removeBodyColor (event) {
+ clearInterval(timerId);
+ startBtn.disabled = false;
+};
 
